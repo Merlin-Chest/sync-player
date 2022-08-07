a87c9fce29ed10252976dbc16c63cbded573169a
鸭脖yaboo <863176846@qq.com>
2022-08-07 09:06:46 +0800

    Extract the configuration

--- client/script/main.js
+++ client/script/main.js
@@ -17,15 +17,7 @@ const App = new Vue({
       time: '',
     },
     userId: '',
-    // goEasy 添加以下变量
-    channel: 'channel1', // GoEasy channel
-    appkey: '******', // GoEasy应用appkey，替换成你的appkey
-
-    // leancloud-realtime 添加以下变量，appId、appKey、server这几个值去leancloud控制台>设置>应用凭证里面找
-    chatRoom: null,
-    appId: 'vQW0I5lmCU0yXXTYMH86XRoH-gzGzoHsz',
-    appKey: 'rYJVNBxN7EPrjKJHnl9CJqAV',
-    server: 'https://vqw0i5lm.lc-cn-n1-shared.com', // REST API 服务器地址
+    ...userConfig
   },
   methods: {
     randomString(length) {
